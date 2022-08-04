25bdb3b2352b401d768a53d63306fb1e1496ff57
Daniel Castro <castrodd@gmail.com>
2022-08-03 17:35:00 -0700

    Add app.js

--- part4/app.js
+++ part4/app.js
@@ -2,14 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
-const password = encodeURIComponent(process.env.MONGODB_PASSWORD)
+const password = config.PASSWORD
 const mongoUrl = `mongodb+srv://modernwebmongodb:${password}@cluster0.tdqhuhf.mongodb.net/blogApp?retryWrites=true&w=majority`
 
-console.log('Connecting to MongoDB...')
+logger.info('Connecting to MongoDB...')
 mongoose.connect(mongoUrl)
   .then( () => {
     logger.info('Connected to MongoDB')
@@ -20,6 +21,7 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- part4/index.js
+++ part4/index.js
@@ -1,32 +1,10 @@
-require('dotenv').config()
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
-
-const password = config.PASSWORD
-const mongoUrl = `mongodb+srv://modernwebmongodb:${password}@cluster0.tdqhuhf.mongodb.net/blogApp?retryWrites=true&w=majority`
-
-logger.info('Connecting to MongoDB...')
-mongoose.connect(mongoUrl)
-  .then( () => {
-    logger.info('Connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.info('Error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
