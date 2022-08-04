91f2e5c6487ff9a1607af34312cf59c3c180d4b4
Daniel Castro <castrodd@gmail.com>
2022-08-03 19:33:45 -0700

    Add tests

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,14 @@
 const dummy = (blogs) => 1
+
+const totalLikes = (blogs) => {
+    if (blogs.length > 0) {
+        return blogs.reduce((prev,curr) => prev + curr.likes, 0)
+    }
+
+    return 0
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
