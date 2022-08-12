e8880531d7e366d5a267ebe652433943ccf3cc6e
Daniel Castro <castrodd@gmail.com>
2022-08-11 18:54:05 -0700

    Username validations

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -1,28 +1,32 @@
 const bcrypt = require('bcrypt')
-const { request, response } = require('../app')
 const usersRouter = require('express').Router()
 const User = require('../models/users')
 
-usersRouter.get('/', async (request, response) => {
+usersRouter.get('/', async (_, response) => {
     const users = await User.find({})
     response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+  try {
+    const { username, name, password } = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-
-  const user = new User({
-    username,
-    name,
-    passwordHash
-  })
-
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+  
+    const user = new User({
+      username,
+      name,
+      passwordHash
+    })
+  
+    const savedUser = await user.save()
+  
+    response.status(201).json(savedUser)
+  } catch (error) {
+    response.status(400).send('User cannot be created.')
+  }
+  
 })
 
 module.exports = usersRouter
