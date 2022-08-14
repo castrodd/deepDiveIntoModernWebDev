31df46d0a3e3479f4d119f88c149ab6d9006dad1
Daniel Castro <castrodd@gmail.com>
2022-08-13 19:27:11 -0700

    Implemented token-based authentication

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 
 logger.info('Connecting to MongoDB...')
@@ -21,5 +22,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
