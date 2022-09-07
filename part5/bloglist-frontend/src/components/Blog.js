70db729918b605a719e8347bb39a618b9880dd4c
Daniel Castro <castrodd@gmail.com>
2022-09-07 16:31:33 -0700

    Like button

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,22 @@
 import { useState } from "react"
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
   const [viewAll, setViewAll] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
+
+  const addLike = async (event) => {
+    event.preventDefault()
+    const blogPayload = {
+      author: blog.author,
+      title: blog.title,
+      url: blog.url,
+      likes: likes + 1,
+      user: blog.user.id
+    }
+    await blogService.create(blogPayload)
+    setLikes(likes + 1)
+  }
 
   const blogStyle = {
     paddingTop: 10,
@@ -24,8 +39,8 @@ const Blog = ({ blog }) => {
       <button onClick={() => setViewAll(false)}>hide</button><br/>
       <span>By:</span> {blog.author}<br/>
       <span> URL:</span> {blog.url}<br/>
-      <span>Likes:</span> {blog.likes}
-      <button>Like</button>
+      <span>Likes:</span> {likes}
+      <button onClick={addLike}>Like</button>
     </div>
   )
 
