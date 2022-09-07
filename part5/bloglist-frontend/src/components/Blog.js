7ffd00da913bc40ec03f9da9dcc0e81929339d6a
Daniel Castro <castrodd@gmail.com>
2022-09-07 16:04:20 -0700

    Toggle individual blogs

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div className="blog">
-    <span>Title:</span> {blog.title}  <span>By:</span> {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [viewAll, setViewAll] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const title = () => (
+    <div className="blog">
+      <span>Title:</span> {blog.title}
+      <button onClick={() => setViewAll(true)}>view</button>
+    </div>
+  )
+
+  const fullBlog = () => (
+    <div className="blog" style={blogStyle}>
+      <span>Title:</span> {blog.title}
+      <button onClick={() => setViewAll(false)}>hide</button><br/>
+      <span>By:</span> {blog.author}<br/>
+      <span> URL:</span> {blog.url}<br/>
+      <span>Likes:</span> {blog.likes}
+      <button>Like</button>
+    </div>
+  )
+
+  return (
+    viewAll
+    ? fullBlog()
+    : title()
+  )
+}
 
 export default Blog
