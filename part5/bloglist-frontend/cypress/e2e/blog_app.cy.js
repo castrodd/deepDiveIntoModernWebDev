d4fd08ad6f77e6104d419badddeae4c88d4c94e7
Daniel Castro <castrodd@gmail.com>
2022-09-11 12:26:01 -0700

    Fix url for testing

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Dwyane Wade',
+      username: 'dwyanewade',
+      password: 'theflash'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
