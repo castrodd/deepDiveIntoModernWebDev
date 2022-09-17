07e15fb036c7735235b1fc3f61b631feb381b0d7
Daniel Castro <castrodd@gmail.com>
2022-09-17 09:37:28 -0700

    Minor clean up

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { legacy_createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+const store = legacy_createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
