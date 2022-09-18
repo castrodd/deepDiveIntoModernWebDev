020fc4035ca379a805356917bf63f0f2f0ba1418
Daniel Castro <castrodd@gmail.com>
2022-09-18 09:24:20 -0700

    Hide notification when no message

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -2,11 +2,19 @@ import { useSelector } from 'react-redux'
 
 const Notification = () => {
   const notification = useSelector(state => state.notifications)
-  const style = {
+  
+  const presentStyle = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+  
+  const absentStyle = {
+    display: 'none'
+  }
+
+  const style = notification ? presentStyle : absentStyle
+
   return (
     <div style={style}>
       {notification}
