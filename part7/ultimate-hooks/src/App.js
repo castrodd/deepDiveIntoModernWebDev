4637b4a5e62f3ebcc83889a56999bdc5f4651174
Daniel Castro <castrodd@gmail.com>
2022-09-26 19:13:40 -0700

    Exercise 7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,16 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
 
-  const create = (resource) => {
-    // ...
+  getAll()
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    return response.data
   }
 
   const service = {
