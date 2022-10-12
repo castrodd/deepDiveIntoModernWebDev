22d8ead1f8b1e9e46e51f0bc0dff92c24fd269e7
Daniel Castro <castrodd@gmail.com>
2022-10-12 15:43:00 -0700

    Fix keys in User component

--- part7/redux-blogs/frontend/src/components/User.js
+++ part7/redux-blogs/frontend/src/components/User.js
@@ -2,14 +2,20 @@ import { useParams } from 'react-router-dom'
 
 const User = ({ blogs }) => {
   const id = useParams().id
+
+  if (!blogs.length) {
+    return <div>Data not loaded. Navigate from Users tab.</div>
+  }
+
   const blogsByUser = blogs.filter(blog => blog.user.id === id)
   const userName = blogsByUser[0].user.name
+
   return (
     <div>
       <h1>{userName}</h1>
       <h2>Added Blogs</h2>
       <ul>
-        {blogsByUser.map(blog => <li key={blog.id}>{blog.title}</li>)}
+        {blogsByUser.map(blog => <li key={blog.title}>{blog.title}</li> )}
       </ul>
     </div>
   )}
