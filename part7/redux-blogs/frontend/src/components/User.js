a28ca853f4a85f33cb3452f17d069d7369dcca5f
Daniel Castro <castrodd@gmail.com>
2022-10-12 15:12:55 -0700

    Add user name to User component

--- part7/redux-blogs/frontend/src/components/User.js
+++ part7/redux-blogs/frontend/src/components/User.js
@@ -3,8 +3,11 @@ import { useParams } from 'react-router-dom'
 const User = ({ blogs }) => {
   const id = useParams().id
   const blogsByUser = blogs.filter(blog => blog.user.id === id)
+  const userName = blogsByUser[0].user.name
   return (
     <div>
+      <h1>{userName}</h1>
+      <h2>Added Blogs</h2>
       <ul>
         {blogsByUser.map(blog => <li key={blog.id}>{blog.title}</li>)}
       </ul>
