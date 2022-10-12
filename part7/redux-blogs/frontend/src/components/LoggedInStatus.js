7ba031dd527d34354320afed51237c7a6f61d6d2
Daniel Castro <castrodd@gmail.com>
2022-10-12 15:09:42 -0700

    Fix onClick in loggedInStatus

--- part7/redux-blogs/frontend/src/components/LoggedInStatus.js
+++ part7/redux-blogs/frontend/src/components/LoggedInStatus.js
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux'
 
-const LoggedInStatus = (handleLogout) => {
+const LoggedInStatus = ({ handleLogout }) => {
   const user = useSelector(state => state.user)
 
   return (
