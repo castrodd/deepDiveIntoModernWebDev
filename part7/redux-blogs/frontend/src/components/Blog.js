d3859f2861dad11c029be4c9c77bc4f6e11cded1
Daniel Castro <castrodd@gmail.com>
2022-10-13 18:27:43 -0700

    Adding comments works

--- part7/redux-blogs/frontend/src/components/Blog.js
+++ part7/redux-blogs/frontend/src/components/Blog.js
@@ -14,7 +14,22 @@ const Blog = ({ modifyBlog, deleteBlog }) => {
       title: blog.title,
       url: blog.url,
       likes: blog.likes + 1,
-      user: blog.user.id
+      user: blog.user.id,
+      comments: blog.comments
+    }
+    await modifyBlog(blog._id, blogPayload)
+  }
+
+  const addComment = async (event) => {
+    event.preventDefault()
+    const newComments = [...blog.comments, event.target['comment'].value]
+    const blogPayload = {
+      author: blog.author,
+      title: blog.title,
+      url: blog.url,
+      likes: blog.likes,
+      user: blog.user.id,
+      comments: newComments
     }
     await modifyBlog(blog._id, blogPayload)
   }
@@ -44,6 +59,15 @@ const Blog = ({ modifyBlog, deleteBlog }) => {
       <button id='remove-button' onClick={removeBlog}>Remove</button>
 
       <h3>Comments</h3>
+      <div>
+        <form onSubmit={addComment}>
+          <input
+            type='text'
+            id='comment'
+          />
+          <button id='submit' type='submit'>Add comment</button>
+        </form>
+      </div>
 
       {blog.comments.length > 0 &&
         < div >
