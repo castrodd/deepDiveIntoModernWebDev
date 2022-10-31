8b9e1aa434918888a46802200642d94296ecd94d
Daniel Castro <castrodd@gmail.com>
2022-10-31 14:32:38 -0700

    Update Book type

--- part8/gQL/library.js
+++ part8/gQL/library.js
@@ -91,11 +91,11 @@ let books = [
 
 const typeDefs = gql`
   type Book {
-    title: String
-    published: Int
-    author: String
-    id: String
-    genres: [String]
+    title: String!
+    published: Int!
+    author: Author!
+    id: ID!
+    genres: [String!]!
   }
 
   type Author {
