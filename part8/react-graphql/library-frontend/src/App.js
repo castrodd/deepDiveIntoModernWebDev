27b6b28b6e820764c8618f2f73e7d1a1bceeecc5
Daniel Castro <castrodd@gmail.com>
2022-11-09 19:40:11 -0800

    Set headers on client

--- part8/react-graphql/library-frontend/src/App.js
+++ part8/react-graphql/library-frontend/src/App.js
@@ -3,15 +3,18 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import Login from './components/Login'
 import NewBook from './components/NewBook'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const client = useApolloClient()
 
   const logout = () => {
     setToken(null)
     localStorage.clear()
+    client.resetStore()
   }
 
   return (
@@ -20,11 +23,14 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+
         {token 
           ? <button onClick={() => logout()}>logout</button>
           : <button onClick={() => setPage('login')}>login</button>
         }
       </div>
+
+      {errorMessage && <p>{errorMessage}</p>}
 
       <Authors show={page === 'authors'} />
       <Books show={page === 'books'} />
