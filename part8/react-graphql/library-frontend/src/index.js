a3f0ddbc1bd8ec928c242aed5dc04cd57c5a1d42
Daniel Castro <castrodd@gmail.com>
2022-11-12 13:56:01 -0800

    Add splitLink to client

--- part8/react-graphql/library-frontend/src/index.js
+++ part8/react-graphql/library-frontend/src/index.js
@@ -7,8 +7,12 @@ import {
   ApolloLink,
   ApolloProvider,
   HttpLink, 
-  InMemoryCache 
+  InMemoryCache,
+  split 
 } from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 
 const authLink = setContext((_, {headers}) => {
   const token = localStorage.getItem('library-user-token')
@@ -22,9 +26,27 @@ const authLink = setContext((_, {headers}) => {
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:4000',
+  })
+)
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: ApolloLink.from([authLink, httpLink])
+  link: splitLink
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
