f8c1f7f1a3de55abee2bc666a72bfd7551164f67
Daniel Castro <castrodd@gmail.com>
2022-11-19 14:36:31 -0800

    Fix eslint issues

--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,41 +1,41 @@
-import express from 'express'
-import { calculateBmi } from './bmiCalculator'
-const app = express()
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+const app = express();
 
 app.get('/hello', (_, res) => {
-  res.send('Hello, Full Stack!')
-})
+  res.send('Hello, Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  let {height, weight} = req.query
+  const {height, weight} = req.query;
   const errorResponse = {
     error: "malformatted parameters"
-  }
+  };
 
   if (height == null || weight == null) {
-    res.send(errorResponse)
-    return
+    res.send(errorResponse);
+    return;
   }
 
-  let heightValue = Number(height)
-  let weightValue = Number(weight)
+  const heightValue = Number(height);
+  const weightValue = Number(weight);
 
   if (isNaN(heightValue) || isNaN(weightValue)) {
-    res.send(errorResponse)
-    return
+    res.send(errorResponse);
+    return;
   }
 
-  let bmiResult = calculateBmi(Number(height), Number(weight))
-  let response = {
+  const bmiResult = calculateBmi(Number(height), Number(weight));
+  const response = {
     height: Number(height),
     weight: Number(weight),
     bmi: bmiResult
-  }
-  res.send(response)
-})
+  };
+  res.send(response);
+});
 
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
