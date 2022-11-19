a0fbd118049bd39daafff1d132e5d67372f59613
Daniel Castro <castrodd@gmail.com>
2022-11-18 17:52:23 -0800

    Exercise 9.5

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -2,23 +2,23 @@ type BmiScore = "Normal (healthy weight)"
   | "Overweight (unhealthy weight)"
   | "Obese (very unhealthy weight)";
 
-const calculateBmi = (height: number, weight: number): BmiScore => {
+export const calculateBmi = (height: number, weight: number): BmiScore => {
   let bmi = weight/(height/100)**2
   if (bmi < 25) return "Normal (healthy weight)"
   if (bmi < 30) return "Overweight (unhealthy weight)"
   return "Obese (very unhealthy weight)"
 }
 
-if (process.argv.length < 4) {
-  throw new Error("Requires a height and weight!")
-}
+// if (process.argv.length < 4) {
+//   throw new Error("Requires a height and weight!")
+// }
 
-if (process.argv.length > 4) {
-  throw new Error("Too many arguments!")
-}
+// if (process.argv.length > 4) {
+//   throw new Error("Too many arguments!")
+// }
 
-const height = Number(process.argv[2])
-const weight = Number(process.argv[3])
+// const height = Number(process.argv[2])
+// const weight = Number(process.argv[3])
 
-console.log("Height: ", height, " Weight: ", weight)
-console.log(calculateBmi(height, weight))
+// console.log("Height: ", height, " Weight: ", weight)
+// console.log(calculateBmi(height, weight))
--- part9/exercises/index.ts
+++ part9/exercises/index.ts
@@ -1,12 +1,41 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+import { calculateBmi } from './bmiCalculator'
+const app = express()
 
 app.get('/hello', (_, res) => {
-  res.send('Hello, Full Stack!');
-});
+  res.send('Hello, Full Stack!')
+})
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  let {height, weight} = req.query
+  const errorResponse = {
+    error: "malformatted parameters"
+  }
+
+  if (height == null || weight == null) {
+    res.send(errorResponse)
+    return
+  }
+
+  let heightValue = Number(height)
+  let weightValue = Number(weight)
+
+  if (isNaN(heightValue) || isNaN(weightValue)) {
+    res.send(errorResponse)
+    return
+  }
+
+  let bmiResult = calculateBmi(Number(height), Number(weight))
+  let response = {
+    height: Number(height),
+    weight: Number(weight),
+    bmi: bmiResult
+  }
+  res.send(response)
+})
+
+const PORT = 3003
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
