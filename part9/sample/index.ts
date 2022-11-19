2e8b660cadd81f73dc446c0d5f0dd6e7ec840005
Daniel Castro <castrodd@gmail.com>
2022-11-18 21:05:57 -0800

    Fix all eslint errors

--- part9/sample/index.ts
+++ part9/sample/index.ts
@@ -11,9 +11,23 @@ app.get('/pong', (_, res) => {
 });
 
 app.post('/calculate', (req, res) => {
+   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment 
   const { value1, value2, op } = req.body;
 
-  const result = multiplicator(value1, value2, op);
+  if ( !value1 || isNaN(Number(value1))) {
+    return res.status(400).send({ error: 'Value is not a number.'});
+  }
+
+  if ( !value2 || isNaN(Number(value2))) {
+    return res.status(400).send({ error: 'Value is not a number.'});
+  }
+
+  if ( !op ) {
+    return res.status(400).send({ error: 'No operation argument.'});
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = multiplicator(Number(value1), Number(value2), op);
   res.send(result);
 });
 
