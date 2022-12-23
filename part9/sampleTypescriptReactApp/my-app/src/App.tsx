bad5b7059dd101a07f36331a1bbc21691a96462a
Daniel Castro <castrodd@gmail.com>
2022-12-23 14:01:04 -0800

    Slight refactor

--- part9/sampleTypescriptReactApp/my-app/src/App.tsx
+++ part9/sampleTypescriptReactApp/my-app/src/App.tsx
@@ -1,24 +1,9 @@
 import Content from './components/Content';
 import Header from './components/Header';
 import Total from './components/Total';
+import { courseName, courseParts } from './data/Course';
 
 const App = () => {
-  const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
-
   return (
     <div>
       <Header courseName={courseName} />
---
+++ part9/sampleTypescriptReactApp/my-app/src/data/Course.ts
@@ -0,0 +1,15 @@
+export const courseName = "Half Stack application development";
+export const courseParts = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10
+  },
+  {
+    name: "Using props",
+    exerciseCount: 7
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14
+  }
+];
