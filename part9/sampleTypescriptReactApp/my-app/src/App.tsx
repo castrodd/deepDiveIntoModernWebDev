39952229281aaf924fac62788e7c0da736319f27
Daniel Castro <castrodd@gmail.com>
2022-12-23 13:25:38 -0800

    Set up Exercise 9.14

--- part9/sampleTypescriptReactApp/my-app/src/App.tsx
+++ part9/sampleTypescriptReactApp/my-app/src/App.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import './App.css';
-import Welcome from './Welcome';
 
-function App() {
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
   return (
-    <div className="App">
-      <Welcome name="Philip" />
+    <div>
+      <h1>{courseName}</h1>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
     </div>
   );
-}
+};
 
 export default App;
--- part9/sampleTypescriptReactApp/my-app/src/Welcome.tsx
+++
@@ -1,9 +0,0 @@
-interface WelcomeProps {
-  name: string;
-}
-
-const Welcome = (props: WelcomeProps) => {
-  return <h1>Welcome, {props.name}!</h1>;
-}
-
-export default Welcome;
