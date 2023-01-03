189a19ef28137d1786701358196e0fc0434cb5c2
Daniel D. Castro <castrodd@gmail.com>
2023-01-03 15:59:22 -0800

    Add missing semicolons

--- part9/patientor/src/state/reducer.ts
+++ part9/patientor/src/state/reducer.ts
@@ -15,14 +15,14 @@ export const setPatientList = (patientList: Patient[]):Action => {
   return { 
     type: "SET_PATIENT_LIST", 
     payload: patientList 
-  }
+  };
 };
 
 export const addPatient = (newPatient: Patient):Action => {
   return { 
     type: "ADD_PATIENT", 
     payload: newPatient 
-  }
+  };
 };
 
 export const reducer = (state: State, action: Action): State => {
