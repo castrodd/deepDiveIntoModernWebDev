be025ed0e272d26201aabc6c048b1e0d14b27688
Daniel D. Castro <castrodd@gmail.com>
2023-01-03 17:15:07 -0800

    Add Entry type

--- part9/patientor-backend/types.ts
+++ part9/patientor-backend/types.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
-
 export interface Patient {
   id: string;
   name: string;
@@ -28,6 +25,53 @@ export interface PatientEntry {
   occupation: string;
   entries?: Entry[];
 }
+
+interface BaseEntry {
+  id: string,
+  date: string,
+  type: string,
+  specialist: string,
+  diagnosisCodes?: string[],
+  description: string
+}
+
+interface Discharge {
+  date: string,
+  criteria: string
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital",
+  discharge: Discharge
+}
+
+interface SickLeave {
+  startDate: string,
+  endDate: string
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthCare",
+  employerName: string,
+  sickLeave?: SickLeave
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck",
+  healthCheckRating: HealthCheckRating
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export enum Gender {
   Male = "male",
