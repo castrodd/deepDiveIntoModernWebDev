92685f135dfbc878045a4bbf978ba7bbdae59c8f
Daniel D. Castro <castrodd@gmail.com>
2023-01-05 20:20:13 -0800

    Adding diagnosis names to codes (9.21)

--- part9/patientor/src/components/EntriesTable.tsx
+++ part9/patientor/src/components/EntriesTable.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import axios from "axios";
+import { apiBaseUrl } from "../constants";
+import { Diagnosis } from "../types";
 import { Entry } from "../types";
 
 interface EntriesTableProps {
@@ -5,11 +9,56 @@ interface EntriesTableProps {
 }
 
 const EntriesTable = ({ entries }: EntriesTableProps) => {
+    const [diagnoses, setDiagnoses] = React.useState<Diagnosis[]>();
+    const [error, setError] = React.useState<string>();
+    const diagnosesMap: {[key:string]: string} = {};
+
+    const fetchAllDiagnoses = async () => {
+        try {
+            const { data: allDiagnoses } = await axios.get<Diagnosis[]>(
+                `${apiBaseUrl}/diagnoses`
+            );
+
+            setDiagnoses(allDiagnoses);
+        } catch (e: unknown) {
+            if (axios.isAxiosError(e)) {
+                console.error(e?.response?.data || "Unrecognized axios error");
+                setError(String(e?.response?.data?.error) || "Unrecognized axios error");
+            } else {
+                console.error("Unknown error", e);
+                setError("Unknown error");
+            }
+        }
+    };
+
+    React.useEffect(() => {
+        const getDiagnoses = async () => {
+            await fetchAllDiagnoses();
+        };
+
+        void getDiagnoses();
+    });
+
+    if (error) {
+        return (
+            <div>
+                <p>{error}</p>
+            </div>
+        );
+    }
+
     if (entries.length) {
+
+        diagnoses?.forEach(diagnosis => {
+            if (diagnosis) {
+                diagnosesMap[diagnosis.code] = diagnosis.name;
+            }
+        }); 
+
         return (
             <div>
                 <h2>Entries</h2>
-                { entries.map(entry => {
+                {entries.map(entry => {
                     return (
                         <div key={entry.id}>
                             <p>{entry.date}</p>
@@ -17,7 +66,10 @@ const EntriesTable = ({ entries }: EntriesTableProps) => {
                             <ul>
                                 {entry?.diagnosisCodes && entry?.diagnosisCodes.map(code => {
                                     return (
-                                        <li key={code}>{code}</li>
+                                        <li key={code}>
+                                            {code}: {diagnosesMap[code]}
+                                        </li>
+
                                     );
                                 })}
                             </ul>
