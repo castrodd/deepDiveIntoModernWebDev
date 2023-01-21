1d5d96e1389d138f1b05ccc6be4880234d8a2c22
castrodd <castrodd@gmail.com>
2023-01-20 20:03:58 -0800

    Basic styling of entry items

--- part9/patientor/src/components/EntryItem.tsx
+++ part9/patientor/src/components/EntryItem.tsx
@@ -8,19 +8,22 @@ interface EntryItemProps {
 
 export const EntryItem = ({entry, diagnosesMap}: EntryItemProps) => {
     return (
-        <div className="entryItem" key={entry.id}>
-            <p>{entry.date} {<EntryTypeIcon type={entry.type} />}</p>
-            <p>Specialist: {entry.specialist}</p>
-            <p>{entry.description}</p>
-            <ul>
-                {entry?.diagnosisCodes && entry?.diagnosisCodes.map(code => {
-                    return (
-                        <li key={code}>
-                            {code}: {diagnosesMap[code]}
-                        </li>
-                    );
-                })}
-            </ul>
+        <div 
+            className="entryItem"
+            style={{padding: "10px", outlineStyle: "dashed", marginBottom: "10px"}} 
+            key={entry.id}>
+                <p>{entry.date} {<EntryTypeIcon type={entry.type} />}</p>
+                <p>Specialist: {entry.specialist}</p>
+                <p>{entry.description}</p>
+                <ul>
+                    {entry?.diagnosisCodes && entry?.diagnosisCodes.map(code => {
+                        return (
+                            <li key={code}>
+                                {code}: {diagnosesMap[code]}
+                            </li>
+                        );
+                    })}
+                </ul>
         </div>
     );
 };
